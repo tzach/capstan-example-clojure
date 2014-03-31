a691478b2ee290c06afe33a4945e98d892855933
Tzach Livyatan <tzach@cloudius-systems.com>
2014-03-31 19:11:12 +0300

    update description and license on project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject capstan-example-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "demo a simple clojure app on OSv"
+  :license {:name "3-clause BSD license"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main  capstan-example-clojure.core
   :aot [capstan-example-clojure.core]
